73b595dd970468b958a8865cee9f1b9e9ef138d5
rdc-justep <song@justep.com>
2016-08-12 09:46:52 +0800

    优化头像上传

--- Baas/org/PersonAvatar.java
+++ Baas/org/PersonAvatar.java
@@ -1,4 +1,4 @@
-package chat;
+package org;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
@@ -34,14 +35,47 @@ import com.justep.baas.action.ActionContext;
 
 public class PersonAvatar {
 	private static String[] allFiles = { "image/vnd.dwg", "image/vnd.dxf", "image/gif", "image/jp2", "image/jpeg", "image/png", "image/vnd.svf", "image/tiff"};
-	static String docStorePath;
+	/**************************************************************************************************************************/
+	static String docStorePath =  Thread.currentThread().getContextClassLoader().getResource("").getPath() + ".." + File.separator + ".."+ File.separator + "chatHeadPortrait";
+	/**************************************************************************************************************************/
 	static File docStoreDir;
 	private static final String DATASOURCE_X5SYS = "system";
 	private static List<String> list = Arrays.asList(allFiles);
 	
+	/**
+	 * 获取文档存储路径，接受
+	 * */
+	/**************************************************************************************************************************/
+	public static String getDocStorePath(String path){
+		return docStoreDir.getAbsolutePath()+File.separator+path;
+	}
+	
+	public static boolean removeFile(String path){
+		//删除状态
+		boolean state = false;
+		//获取头像文件路径的对象
+		File file = new File( getDocStorePath(path));
+		//获取上一级目录
+		File parentFile = file.getParentFile();
+		
+		//判断是否为文件夹
+		if(parentFile.isDirectory()){
+			//获取文件夹下所有文件列表
+			File[] files = parentFile.listFiles();
+			//遍历删除文件
+			for(File delFile : files){
+				delFile.delete();
+			}
+			state = parentFile.delete();
+		}
+		
+		return state;
+	}
+	/**************************************************************************************************************************/
+	
 	private static void crateDir(){
-		String baasPath = Thread.currentThread().getContextClassLoader().getResource("").getPath() + ".." + File.separator + "..";
-		docStorePath = baasPath + File.separator + "chatHeadPortrait";
+//		String baasPath = Thread.currentThread().getContextClassLoader().getResource("").getPath() + ".." + File.separator + "..";
+//		docStorePath = baasPath + File.separator + "chatHeadPortrait";
 		File file = new File(docStorePath);
 		//兼容以前存储目录
 		if(file.exists() && file.isDirectory()){
@@ -126,7 +160,9 @@ public class PersonAvatar {
 				BufferedInputStream in = new BufferedInputStream(fi.getInputStream());
 				imgName =createImgName(fi.getName());
 				folder = createFolder();
-				FileOutputStream out = new FileOutputStream(new File(docStoreDir.getAbsolutePath()+File.separator+folder+File.separator+imgName));
+				/**************************************************************************************************************************/
+				FileOutputStream out = new FileOutputStream(new File(getDocStorePath(folder+File.separator+imgName)));
+				/**************************************************************************************************************************/
 				BufferedOutputStream output = new BufferedOutputStream(out);
 				Streams.copy(in, output, true);
 			} else if("id".equals(fieldName)){
@@ -138,9 +174,26 @@ public class PersonAvatar {
 			}
 		}
 		if(id!=null&&imgName!=null){
+			
+			String selSql = "select sPhoto from sa_opperson where sID = '"+id+"'";
+			
 			Statement stmt = null;
 			String sql = "update sa_opperson set sPhoto='"+folder+"/"+imgName+"' where sID= '"+id+"'" ;
 			stmt = conn.createStatement();
+			
+			/**************************************************************************************************************************/
+			/*
+			 * 查询用户原有头像文件路径，并删除该文件
+			 * */
+			
+			ResultSet rel = stmt.executeQuery(selSql);
+			String filePath=null;
+			while(rel.next()){
+				filePath = rel.getString("sPhoto");
+			}
+			removeFile(filePath);//删除文件操作
+			/**************************************************************************************************************************/
+			
 			int i = stmt.executeUpdate(sql);
 			if(i>0){
 				if(stmt!=null)
